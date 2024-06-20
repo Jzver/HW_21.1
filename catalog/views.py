a0fe057e6b56a4485a51bbabad74442fe1192182
Rf <jonnyzver78@gmail.com>
2024-06-20 19:36:34 +1000

    исправление ошибок

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView,TemplateView
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
 from catalog.models import Product
 
 
@@ -15,12 +15,6 @@ class ProductDetailView(DetailView):
     template_name = 'product_detail.html'
 
 
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {'product': product}
-#     return render(request, "product_detail.html", context)
-
-
 class ContactsTemplateView(TemplateView):
     template_name = 'catalog/contacts.html'
 
@@ -30,11 +24,3 @@ class ContactsTemplateView(TemplateView):
         message = request.POST.get('message')
         print(name, phone, message)
         return super().get(request, *args, **kwargs)
-# Задание 19.1
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'{name} ({phone}) написал: {message}')
-    return render(request, "contacts.html")
